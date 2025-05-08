b147f8809a9896812fdeda5d6fac9bf94aad6581
gebeto <slavik.nychkalo@gmail.com>
2025-05-08 11:14:44 +0300

    make UI working

--- rounded-coffee-weights/src/main.cpp
+++ rounded-coffee-weights/src/main.cpp
@@ -13,6 +13,13 @@
 Arduino_DataBus *bus = new Arduino_ESP32SPI(4 /* DC */, 10 /* CS */, 1 /* SCK */, 0 /* MOSI */, GFX_NOT_DEFINED /* MISO */);
 Arduino_GFX *gfx = new Arduino_GC9A01(bus, GFX_NOT_DEFINED /* RST */, LV_DISP_ROT_270 /* rotation */, true /* IPS */);
 
+static uint32_t screenWidth = 240;
+static uint32_t screenHeight = 240;
+lv_disp_draw_buf_t draw_buf;
+lv_color_t *disp_draw_buf1;
+lv_color_t *disp_draw_buf2;
+lv_disp_drv_t disp_drv;
+
 /* Display flushing */
 void my_disp_flush(lv_disp_drv_t *disp, const lv_area_t *area, lv_color_t *color_p) {
   uint32_t w = (area->x2 - area->x1 + 1);
@@ -27,20 +34,37 @@ void my_disp_flush(lv_disp_drv_t *disp, const lv_area_t *area, lv_color_t *color
   lv_disp_flush_ready(disp);
 }
 
-
 void setup()
 {
-  Serial.begin(115200);
+  // Serial.begin(115200);
+  Serial.begin(9600);
   pinMode(GFX_BL, OUTPUT);
   digitalWrite(GFX_BL, LOW);
   gfx->begin(80000000);
 
   lv_init();
+  delay(10);
+
+  disp_draw_buf1 = (lv_color_t *)heap_caps_malloc(sizeof(lv_color_t) * screenWidth * screenHeight / 8, MALLOC_CAP_INTERNAL | MALLOC_CAP_8BIT);
+  disp_draw_buf2 = (lv_color_t *)heap_caps_malloc(sizeof(lv_color_t) * screenWidth * screenHeight / 8, MALLOC_CAP_INTERNAL | MALLOC_CAP_8BIT);
+
+  lv_disp_draw_buf_init(&draw_buf, disp_draw_buf1, disp_draw_buf2, screenWidth * screenHeight / 8);
+
+  /* Initialize the display */
+  lv_disp_drv_init(&disp_drv);
+  /* Change the following line to your display resolution */
+  disp_drv.hor_res = screenWidth;
+  disp_drv.ver_res = screenHeight;
+  disp_drv.flush_cb = my_disp_flush;
+  disp_drv.draw_buf = &draw_buf;
+  lv_disp_drv_register(&disp_drv);
+
   ui_init();
 }
 
 void loop()
 {
-  
+  lv_timer_handler();
+  delay(5);
 }
 
